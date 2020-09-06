6a5a733d230b4af436e59ed768b2376cded5c968
Bibin Nahas <bbnnaz@gmail.com>
2020-09-06 23:22:39 +0530

    Split pages

--- App.js
+++ App.js
@@ -6,28 +6,48 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './screens/HomeScreen';
 import DetailScreen from './screens/DetailsScreen';
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const DetailStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+
+const HomeStackScreen = ({ navigation }) => (
+  <HomeStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: '#009387',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    }
+  }}>
+    <HomeStack.Screen name="Home" component={HomeScreen} 
+    options = {{
+      title: 'Overview'
+    }}/>
+  </HomeStack.Navigator>
+);
+
+const DetailStackScreen = ({ navigation }) => (
+  <DetailStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: '#009387',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    }
+  }}>
+    <DetailStack.Screen name="Details" component={DetailScreen} />
+  </DetailStack.Navigator>
+);
 
 const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Details" component={DetailScreen} />
+        <Drawer.Screen name="Home" component={HomeStackScreen} />
+        <Drawer.Screen name="Details" component={DetailStackScreen} />
       </Drawer.Navigator>
-      {/* <Stack.Navigator screenOptions={{
-        headerStyle: {
-          backgroundColor: '#009387',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        }
-      }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailScreen} />
-      </Stack.Navigator> */}
     </NavigationContainer>
   );
 }
