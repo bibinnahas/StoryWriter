b990b3b2a2d7ee26d01ac0336594495dee080244
Bibin Nahas <bbnnaz@gmail.com>
2020-09-20 15:58:45 +0530

    bottom tab implementation

--- App.js
+++ App.js
@@ -1,52 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import MainTabScreen from './screens/MainTabScreen';
 
-import HomeScreen from './screens/HomeScreen';
-import DetailScreen from './screens/DetailsScreen';
-
-const HomeStack = createStackNavigator();
-const DetailStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
-
-const HomeStackScreen = ({ navigation }) => (
-  <HomeStack.Navigator screenOptions={{
-    headerStyle: {
-      backgroundColor: '#009387',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    }
-  }}>
-    <HomeStack.Screen name="Home" component={HomeScreen} 
-    options = {{
-      title: 'Overview'
-    }}/>
-  </HomeStack.Navigator>
-);
-
-const DetailStackScreen = ({ navigation }) => (
-  <DetailStack.Navigator screenOptions={{
-    headerStyle: {
-      backgroundColor: '#009387',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    }
-  }}>
-    <DetailStack.Screen name="Details" component={DetailScreen} />
-  </DetailStack.Navigator>
-);
 
 const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeStackScreen} />
-        <Drawer.Screen name="Details" component={DetailStackScreen} />
+        <Drawer.Screen name="Home" component={MainTabScreen} />
+        {/* <Drawer.Screen name="Details" component={DetailStackScreen} /> */}
       </Drawer.Navigator>
     </NavigationContainer>
   );
