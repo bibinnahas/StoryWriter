02e9084b5e6a474de938d4f0c7ee94881b1bf1f1
Bibin Nahas <bbnnaz@gmail.com>
2020-09-20 23:06:47 +0530

    bottom tab implementation

--- screens/MainTabScreen.js
+++ screens/MainTabScreen.js
@@ -12,20 +12,23 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 const HomeStack = createStackNavigator();
 const DetailStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+const ExploreStack = createStackNavigator();
 
 const Tab = createMaterialBottomTabNavigator();
 
 const MainTabScreen = () => (
     <Tab.Navigator
         initialRouteName="Home"
-        activeColor="#e91e63"
-        style={{ backgroundColor: 'tomato' }}
+        activeColor="#fff"
+        // style={{ backgroundColor: 'tomato' }}
     >
         <Tab.Screen
             name="Home"
             component={HomeStackScreen}
             options={{
                 tabBarLabel: 'Home',
+                tabBarColor: '#009387',
                 tabBarIcon: ({ color }) => (
                     <Icon name="ios-home" color={color} size={26} />
                 ),
@@ -35,7 +38,8 @@ const MainTabScreen = () => (
             name="Notifications"
             component={DetailStackScreen}
             options={{
-                tabBarLabel: 'Updates',
+                tabBarLabel: 'Notifications',
+                tabBarColor: '#009387',
                 tabBarIcon: ({ color }) => (
                     <Icon name="ios-notifications" color={color} size={26} />
                 ),
@@ -43,9 +47,10 @@ const MainTabScreen = () => (
         />
         <Tab.Screen
             name="Profile"
-            component={ProfileScreen}
+            component={ProfileStackScreen}
             options={{
                 tabBarLabel: 'Profile',
+                tabBarColor: '#009387',
                 tabBarIcon: ({ color }) => (
                     <Icon name="ios-person" color={color} size={26} />
                 ),
@@ -53,9 +58,10 @@ const MainTabScreen = () => (
         />
         <Tab.Screen
             name="ExploreScreen"
-            component={ExploreScreen}
+            component={ExploreStackScreen}
             options={{
                 tabBarLabel: 'Explore',
+                tabBarColor: '#009387',
                 tabBarIcon: ({ color }) => (
                     <Icon name="ios-aperture" color={color} size={26} />
                 ),
@@ -74,7 +80,7 @@ const HomeStackScreen = ({ navigation }) => (
         headerTintColor: '#fff',
         headerTitleStyle: {
             fontWeight: 'bold',
-        }
+        } 
     }}>
         <HomeStack.Screen name="Home" component={HomeScreen}
             options={{
@@ -109,4 +115,48 @@ const DetailStackScreen = ({ navigation }) => (
                         }></Icon.Button>)
             }} />
     </DetailStack.Navigator>
+);
+
+const ProfileStackScreen = ({ navigation }) => (
+    <ProfileStack.Navigator screenOptions={{
+        headerStyle: {
+            backgroundColor: '#009387',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        }
+    }}>
+        <ProfileStack.Screen name="Profile" component={ProfileScreen}
+            options={{
+                headerLeft: () => (
+                    <Icon.Button name="anfroid-menu" size={25}
+                        backgroundColor="#009387" onPress={() =>
+                            navigation.
+                                openDrawer()
+                        }></Icon.Button>)
+            }} />
+    </ProfileStack.Navigator>
+);
+
+const ExploreStackScreen = ({ navigation }) => (
+    <ExploreStack.Navigator screenOptions={{
+        headerStyle: {
+            backgroundColor: '#009387',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        }
+    }}>
+        <ExploreStack.Screen name="Explore" component={DetailScreen}
+            options={{
+                headerLeft: () => (
+                    <Icon.Button name="anfroid-menu" size={25}
+                        backgroundColor="#009387" onPress={() =>
+                            navigation.
+                                openDrawer()
+                        }></Icon.Button>)
+            }} />
+    </ExploreStack.Navigator>
 );
