9d992df366de4e4a0c5a68a62435b3d457f35141
Bibin Nahas <bbnnaz@gmail.com>
2020-09-22 10:59:14 +0530

    Decorative work

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView } from '@react-navigation/drawer';
 import MainTabScreen from './screens/MainTabScreen';
+
+import { DrawerContent } from './screens/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator drawerContent={props => <DrawerContent{...props} />}>
         <Drawer.Screen name="Home" component={MainTabScreen} />
         {/* <Drawer.Screen name="Details" component={DetailStackScreen} /> */}
       </Drawer.Navigator>
